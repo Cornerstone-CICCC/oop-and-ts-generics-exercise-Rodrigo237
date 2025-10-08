18a3a913baab455a5f6945bf4f80b1e156f8f487
Rodrigo237 <36214941+Rodrigo237@users.noreply.github.com>
2025-10-08 13:29:34 -0700

    Exercise done

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,22 +1,18 @@
+"use strict";
 // 4. Create a generic class called `Container` that stores a value of any type.
 // Add methods to `getValue` and `setValue`.
 // Initialize it with "Initial Value", update it to "Updated Value", and log the new value.
-
-class Container<T> {
-  private value: T;
-
-  constructor(value: T) {
-    this.value = value;
-  }
-
-  getValue(): T{
-    return this.value;
-  }
-  setValue = (value: T):void => {
-    this.value = value;
-  };
+class Container {
+    constructor(value) {
+        this.setValue = (value) => {
+            this.value = value;
+        };
+        this.value = value;
+    }
+    getValue() {
+        return this.value;
+    }
 }
-
 const stringContainer = new Container("Initial Value");
 stringContainer.setValue("Updated Value");
 console.log(stringContainer.getValue()); // Expected output: "Updated Value"
